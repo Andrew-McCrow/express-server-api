16fcb11b52355e9903ec803adc89af8a57da14a0
Andrew-McCrow <andrewmccrow@gmail.com>
2026-02-27 17:11:29 +0000

    worked thorugh exercise but gave up as it was just refactoring in MVC archtecture

--- src/index.js
+++ src/index.js
@@ -1,13 +1,59 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
+import models from "./models";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
+});
+
+app.get("/users", (req, res) => {
+  return res.send(Object.values(users));
+});
+
+app.get("/users/:userId", (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+
+app.get("/messages", (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get("/messages/:messageId", (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+app.post("/messages", (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id,
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
+});
+
+app.delete("/messages/:messageId", (req, res) => {
+  const { [req.params.messageId]: message, ...otherMessages } = messages;
+
+  messages = otherMessages;
+
+  return res.send(message);
 });
 
 app.listen(process.env.PORT, () =>
